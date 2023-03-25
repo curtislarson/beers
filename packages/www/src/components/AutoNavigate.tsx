d4755fdd4f258b20acddcfc334dc07d90bc2f7c9
Curtis Larson <quackware@gmail.com>
2023-03-25 12:58:14 -0400

    chore: remove auto navigate for now until implemented, rename Filter By Location to Filter by Trip

--- packages/www/src/components/AutoNavigate.tsx
+++ packages/www/src/components/AutoNavigate.tsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 export default function AutoNavigate() {
   const [autoNavigate, toggleAutoNavigate] = useState(false);
 
-  return (
-    <button
-      className="btn-xs btn w-full bg-base-300 normal-case sm:btn-md"
-      onClick={() => toggleAutoNavigate(!autoNavigate)}
-    >
-      {autoNavigate ? "Navigating..." : "Start Auto Navigate"}
-    </button>
-  );
+  return null;
+  // return (
+  //   <button
+  //     className="btn-xs btn w-full bg-base-300 normal-case sm:btn-md"
+  //     onClick={() => toggleAutoNavigate(!autoNavigate)}
+  //   >
+  //     {autoNavigate ? "Navigating..." : "Start Auto Navigate"}
+  //   </button>
+  // );
 }
